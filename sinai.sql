6e95d6c98165c4e4693ef7437ba269395117b271
Rudiyant <rudiiyantt@gmail.com>
2020-03-05 02:42:09 +0700

    upload file db

--- sinai.sql
+++ sinai.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 31 Jan 2020 pada 11.45
--- Versi server: 10.4.10-MariaDB
--- Versi PHP: 7.3.12
+-- Waktu pembuatan: 04 Mar 2020 pada 20.14
+-- Versi server: 10.4.6-MariaDB
+-- Versi PHP: 7.3.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -35,6 +35,13 @@ CREATE TABLE `admin` (
   `password` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data untuk tabel `admin`
+--
+
+INSERT INTO `admin` (`id_admin`, `nama`, `username`, `password`) VALUES
+('891c0775-3b48-11ea-b233-b42e9929de51', 'Admin', '0100501', '11111');
+
 -- --------------------------------------------------------
 
 --
@@ -45,6 +52,7 @@ CREATE TABLE `direktur` (
   `niy` char(17) NOT NULL,
   `nama` varchar(60) NOT NULL,
   `jabatan` varchar(50) NOT NULL,
+  `divisi` varchar(50) NOT NULL,
   `status` int(11) NOT NULL,
   `id_karyawan` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -58,17 +66,25 @@ CREATE TABLE `direktur` (
 CREATE TABLE `karyawan` (
   `id_karyawan` varchar(50) NOT NULL,
   `nama` varchar(60) NOT NULL,
-  `nik` char(16) NOT NULL,
-  `niy` char(17) NOT NULL,
+  `nik` varchar(50) NOT NULL,
+  `niy` varchar(50) NOT NULL,
   `nama_divisi` varchar(50) NOT NULL,
-  `id_divisi` varchar(50) NOT NULL,
   `nama_jabatan` varchar(50) NOT NULL,
-  `id_jabatan` varchar(50) NOT NULL,
   `is_active` int(11) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `updated_at` datetime NOT NULL DEFAULT current_timestamp(),
   `akun_id` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `karyawan`
+--
+
+INSERT INTO `karyawan` (`id_karyawan`, `nama`, `nik`, `niy`, `nama_divisi`, `nama_jabatan`, `is_active`, `created_at`, `updated_at`, `akun_id`) VALUES
+('265c0775-3b48-11ea-b233-b42e9929de55', 'M. Saepullah', '333016600002', '', 'Divisi IT', 'Magang IT', 1, '2020-03-05 00:00:00', '2020-03-05 00:00:00', '265c0775-3b48-11ea-b233-b42e9929de55'),
+('484c0775-3b48-11ea-b233-b42e9929de53', 'Johan Saifudin', '', '02 01 1019 090752', 'SD Teladan', 'Guru Kelas', 1, '2020-03-05 00:00:00', '2020-03-05 00:00:00', '484b59f1-3b48-11ea-b233-b42e9929de53'),
+('52f4f0c6-3b48-11ea-b233-b42e9929de53', 'Imam Prasetyo, S.Kom.', '', '02 01 1019 210989', 'Divisi IT', 'Staff IT', 1, '2020-03-05 00:00:00', '2020-03-05 00:00:00', '52f49ae0-3b48-11ea-b233-b42e9929de53'),
+('d8e7a282-3b47-11ea-b233-b42e9929de53', 'Muhammad Prasetyo', '', '02 01 1016 210989', 'Operasional', 'Direktur', 1, '2020-03-05 00:00:00', '2020-03-05 00:00:00', 'd8e75512-3b47-11ea-b233-b42e9929de53');
 
 -- --------------------------------------------------------
 
@@ -85,6 +101,16 @@ CREATE TABLE `karyawan_akun` (
   `is_active` char(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data untuk tabel `karyawan_akun`
+--
+
+INSERT INTO `karyawan_akun` (`akun_id`, `akun_username`, `akun_password`, `created_at`, `updated_at`, `is_active`) VALUES
+('265c0775-3b48-11ea-b233-b42e9929de55', '0100401', '11111', '2020-03-05 00:00:00', '2020-03-05 00:00:00', '1'),
+('484b59f1-3b48-11ea-b233-b42e9929de53', '0100201', '11111', '2020-03-05 00:00:00', '2020-03-05 00:00:00', '1'),
+('52f49ae0-3b48-11ea-b233-b42e9929de53', '0100301', '11111', '2020-03-05 00:00:00', '2020-03-05 00:00:00', '1'),
+('d8e75512-3b47-11ea-b233-b42e9929de53', '0100101', '11111', '2020-03-05 00:00:00', '2020-03-05 00:00:00', '1');
+
 -- --------------------------------------------------------
 
 --
@@ -93,15 +119,25 @@ CREATE TABLE `karyawan_akun` (
 
 CREATE TABLE `surat_cuti` (
   `alasan_cuti` varchar(60) NOT NULL,
-  `hari_tgl_mulai` date NOT NULL,
-  `hari_tgl_selesai` date NOT NULL,
-  `hari_tgl_masuk` date NOT NULL,
-  `tujuan_cuti` int(11) NOT NULL,
+  `hari_tgl_mulai` varchar(50) NOT NULL,
+  `hari_tgl_selesai` varchar(50) NOT NULL,
+  `hari_tgl_masuk` varchar(50) NOT NULL,
+  `tujuan_cuti` varchar(50) NOT NULL,
   `status_cuti` int(11) NOT NULL,
-  `tanggal` date NOT NULL,
+  `keterangan` varchar(100) NOT NULL,
+  `tanggal` varchar(50) NOT NULL,
   `id_karyawan` varchar(50) NOT NULL,
+  `alamat_karyawan` varchar(50) NOT NULL,
   `nomor_surat` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `surat_cuti`
+--
+
+INSERT INTO `surat_cuti` (`alasan_cuti`, `hari_tgl_mulai`, `hari_tgl_selesai`, `hari_tgl_masuk`, `tujuan_cuti`, `status_cuti`, `keterangan`, `tanggal`, `id_karyawan`, `alamat_karyawan`, `nomor_surat`) VALUES
+('Liburan keluarga ke luar negeri', 'Kamis, 05 Maret 2020', 'Minggu, 08 Maret 2020', 'Senin, 09 Maret 2020', 'Direktur Sekolah Teladan', 1, '', '04 Maret 2020', '484c0775-3b48-11ea-b233-b42e9929de53', 'Condongcatur, Depok Sleman, Yogyakarta', '01/SIC/III/2020'),
+('Liburan ke China', 'Kamis, 05 Maret 2020', 'Sabtu, 14 Maret 2020', 'Senin, 16 Maret 2020', 'Direktur Sekolah Teladan', 2, 'Permohnan cuti terlalu lama', '04 Maret 2020', '52f4f0c6-3b48-11ea-b233-b42e9929de53', 'Bantul, Yogyakarta', '02/SIC/III/2020');
 
 -- --------------------------------------------------------
 
@@ -113,11 +149,20 @@ CREATE TABLE `surat_izin` (
   `id_izin` varchar(50) NOT NULL,
   `keterangan_izin` varchar(60) NOT NULL,
   `alasan_izin` varchar(60) NOT NULL,
-  `hari_tanggal` date NOT NULL,
+  `hari_tanggal` varchar(50) NOT NULL,
   `lama_waktu_izin` varchar(20) NOT NULL,
   `status_izin` int(11) NOT NULL,
   `id_karyawan` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `surat_izin`
+--
+
+INSERT INTO `surat_izin` (`id_izin`, `keterangan_izin`, `alasan_izin`, `hari_tanggal`, `lama_waktu_izin`, `status_izin`, `id_karyawan`) VALUES
+('cNL9B', 'Izin Tidak Masuk', 'Sakit Maag', 'Kamis, 05 Maret 2020', '1 Hari', 0, '52f4f0c6-3b48-11ea-b233-b42e9929de53'),
+('cptjJ', 'Meninggalkan Sekolah saat Jam Kerja', 'Ada rapat di luar sekolah', 'Kamis, 01 Januari 1970', '3 Jam', 0, '484c0775-3b48-11ea-b233-b42e9929de53'),
+('V0PvX', 'Pulang Lebih Awal', 'Kondangan', 'Jumat, 06 Maret 2020', '2 Jam', 0, '265c0775-3b48-11ea-b233-b42e9929de55');
 
 --
 -- Indexes for dumped tables
@@ -141,10 +186,6 @@ ALTER TABLE `direktur`
 --
 ALTER TABLE `karyawan`
   ADD PRIMARY KEY (`id_karyawan`),
-  ADD UNIQUE KEY `nik` (`nik`),
-  ADD UNIQUE KEY `niy` (`niy`),
-  ADD UNIQUE KEY `id_divisi` (`id_divisi`),
-  ADD UNIQUE KEY `id_jabatan` (`id_jabatan`),
   ADD KEY `akun_id` (`akun_id`) USING BTREE;
 
 --
